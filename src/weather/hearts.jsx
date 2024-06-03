b3b71b557d759900f964cc37e3dc209d6ea070ff
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-06-03 00:25:05 -0400

    troubleshooting

--- src/weather/hearts.jsx
+++ src/weather/hearts.jsx
@@ -1,8 +1,24 @@
-const Hearts = () => {
-    
+import { Component } from 'react';
+import Confetti from 'react-confetti';
+import heart from '../assets/weather/heart.svg'
+
+class Hearts extends Component {
+  render() {
+
     return (
-      <p>Hearts</p>
+      <div className="confetti-container">
+        <Confetti
+          width={1080}
+          drawShape={heart}
+          colors={['rgba(100, 100, 200, 0.7)']}
+          gravity={2}
+          angle={0}
+          initialVelocityX={0}
+          wind={0}
+        />
+      </div>
     );
-  };
-  
-  export default Hearts;
+  }
+}
+
+export default Hearts;
